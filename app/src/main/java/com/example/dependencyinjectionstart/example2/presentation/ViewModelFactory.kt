12fbf068b292ecbc2fe0f7570347088fdf133cee
Vitaly <onebeattrue@gmail.com>
2023-08-29 02:01:23 +0300

    Первый способ инъекции параметров в конструктор ViewModel С помощью передачи параметров внутрь ViewModelFactory Очевидный недостаток - много кода при большом количестве ViewModel'ей

---
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/ViewModelFactory.kt
@@ -0,0 +1,22 @@
+package com.example.dependencyinjectionstart.example2.presentation
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.dependencyinjectionstart.example2.domain.ExampleRepository
+import com.example.dependencyinjectionstart.example2.domain.ExampleUseCase
+import javax.inject.Inject
+
+class ViewModelFactory @Inject constructor(
+    private val useCase: ExampleUseCase,
+    private val repository: ExampleRepository
+): ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass == ExampleViewModel::class.java) {
+            return ExampleViewModel(useCase) as T
+        }
+        if (modelClass == ExampleViewModel2::class.java) {
+            return ExampleViewModel2(repository) as T
+        }
+        throw RuntimeException("Unknown ViewModel class: $modelClass")
+    }
+}
