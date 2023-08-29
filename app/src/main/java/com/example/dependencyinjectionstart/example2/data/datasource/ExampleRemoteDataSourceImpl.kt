e2926db2df1609db1c1fd25732aac0407ba1adc9
Vitaly <onebeattrue@gmail.com>
2023-08-29 22:34:44 +0300

    Домашнее задание: Создание "типо" тестового TestExampleRemoteDataSourceImpl" и Qualifier для выбора тестового/настоящего

--- app/src/main/java/com/example/dependencyinjectionstart/example2/data/datasource/ExampleRemoteDataSourceImpl.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/data/datasource/ExampleRemoteDataSourceImpl.kt
@@ -1,5 +1,6 @@
 package com.example.dependencyinjectionstart.example2.data.datasource
 
+import android.util.Log
 import com.example.dependencyinjectionstart.example2.data.network.ExampleApiService
 import javax.inject.Inject
 
@@ -8,6 +9,18 @@ class ExampleRemoteDataSourceImpl @Inject constructor(
 ) : ExampleRemoteDataSource {
 
     override fun method() {
+        Log.d("ExampleRemoteDataSource", "Original")
+        apiService.method()
+    }
+}
+
+// типа тестовый репозиторий
+class TestExampleRemoteDataSourceImpl @Inject constructor(
+    private val apiService: ExampleApiService
+) : ExampleRemoteDataSource {
+
+    override fun method() {
+        Log.d("ExampleRemoteDataSource", "Test")
         apiService.method()
     }
 }
--- app/src/main/java/com/example/dependencyinjectionstart/example2/di/DataModule.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/di/DataModule.kt
@@ -4,6 +4,9 @@ import com.example.dependencyinjectionstart.example2.data.datasource.ExampleLoca
 import com.example.dependencyinjectionstart.example2.data.datasource.ExampleLocalDataSourceImpl
 import com.example.dependencyinjectionstart.example2.data.datasource.ExampleRemoteDataSource
 import com.example.dependencyinjectionstart.example2.data.datasource.ExampleRemoteDataSourceImpl
+import com.example.dependencyinjectionstart.example2.data.datasource.TestExampleRemoteDataSourceImpl
+import com.example.dependencyinjectionstart.example2.di.qualifiers.ExampleRemoteDataSourceQualifier
+import com.example.dependencyinjectionstart.example2.di.qualifiers.TestExampleRemoteDataSourceQualifier
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.migration.DisableInstallInCheck
@@ -16,7 +19,13 @@ interface DataModule {
     @Binds
     fun bindLocalDataSource(impl: ExampleLocalDataSourceImpl): ExampleLocalDataSource
 
+    @ExampleRemoteDataSourceQualifier
     @ApplicationScope
     @Binds
     fun bindRemoteDataSource(impl: ExampleRemoteDataSourceImpl): ExampleRemoteDataSource
+
+    @TestExampleRemoteDataSourceQualifier
+    @ApplicationScope
+    @Binds
+    fun bindTestRemoteDataSource(impl: TestExampleRemoteDataSourceImpl): ExampleRemoteDataSource
 }
